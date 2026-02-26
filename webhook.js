fa86dd48239003a1df4a326f4f8ab4d7d7d8d970
neverknown <gauravkalakho@gmail.com>
2026-02-26 12:05:25 +0530

    'wb'

---
+++ webhook.js
@@ -0,0 +1,41 @@
+const express = require('express')
+var xhub = require('express-x-hub');
+require('dotenv')
+var app = express();
+const webhook = express.Router();
+// app.set('port', (process.env.PORT || 5000));
+
+webhook.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+webhook.use(express.json());
+
+
+
+var token = process.env.TOKEN || 'token';
+var received_updates = [];
+
+webhook.get('/', function (req, res) {
+    // console.log(req);
+    res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+});
+
+webhook.get(['/facebook', '/instagram', '/threads'], function (req, res) {
+    if (
+        req.query['hub.mode'] == 'subscribe' &&
+        req.query['hub.verify_token'] == token
+    ) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(400);
+    }
+});
+webhook.post('/instagram', function (req, res) {
+    console.log('Instagram request body:');
+    console.log(req.body);
+    // Process the Instagram updates here
+    received_updates.unshift(req.body);
+    res.sendStatus(200);
+});
+
+module.exports = {
+    webhook
+}
